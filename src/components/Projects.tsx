2db388d95a95f4357cb984eccfb71b74a77bc7a6
NathanPip <107423247+NathanPip@users.noreply.github.com>
2023-01-18 16:39:30 -0800

    sort projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -6,39 +6,80 @@ const Projects = () => {
   const projectsRes = trpc.projects.useQuery();
 
   //signals
-  const [selectedFilter, setSelectedFilter] = createSignal<"featured" | "github" | "oss">("featured");
-  const featuredClickHandler = (e:MouseEvent) => {
+  const [selectedFilter, setSelectedFilter] = createSignal<
+    "featured" | "github" | "oss"
+  >("featured");
+  const featuredClickHandler = (e: MouseEvent) => {
     const element = e.target as HTMLButtonElement;
     const name = element.innerText.toLowerCase();
     setSelectedFilter(name as "featured" | "github" | "oss");
-  }
+  };
 
   // memos
   const displayedProjects = createMemo(() => {
-    if(selectedFilter() === "featured") {
+    if (selectedFilter() === "featured") {
+      return projectsRes.data
+        ?.filter((project) => project.featured)
+        .sort((a, b) => {
+          const dateA = new Date(a.updated_at).valueOf();
+          const dateB = new Date(b.updated_at).valueOf();
+          return dateA - dateB;
+        })
+        .map((project) => <ProjectCard project={project} />);
+    } else if (selectedFilter() === "github") {
+      return projectsRes.data
+        ?.filter((project) => !project.fork)
+        .sort((a, b) => {
+          const dateA = new Date(a.updated_at).valueOf();
+          const dateB = new Date(b.updated_at).valueOf();
+          const value1 = dateA * 0.000000006 + a.commit_count * 0.5;
+          const value2 = dateB * 0.000000006 + b.commit_count * 0.5;
+          return value2 - value1;
+        })
+        .map((project) => <ProjectCard project={project} />);
+    } else if (selectedFilter() === "oss") {
+      return projectsRes.data
+        ?.filter((project) => project.fork)
+        .map((project) => <ProjectCard project={project} />);
     }
-  })
+  });
 
   // tailwind styles
-  const projectFilterButtonStyles = "mx-2 font-semibold mb-2 bg-stone-300 shadow-md px-2 rounded-md"
-  const projectFilterButtonSelectedStyles = "bg-stone-400"
+  const projectFilterButtonStyles =
+    "mx-2 font-semibold mb-2 bg-stone-300 shadow-md px-2 rounded-md";
+  const projectFilterButtonSelectedStyles = "bg-stone-400";
 
   return (
     <div>
       <div class="flex">
         <h1 class="text-2xl font-semibold mb-2">Projects</h1>
         <div class="ml-auto mt-auto flex justify-around">
-          <button class={`${projectFilterButtonStyles} ${selectedFilter() === "featured" ? projectFilterButtonSelectedStyles : ""}`}
+          <button
+            class={`${projectFilterButtonStyles} ${
+              selectedFilter() === "featured"
+                ? projectFilterButtonSelectedStyles
+                : ""
+            }`}
             onClick={featuredClickHandler}
           >
             Featured
           </button>
-          <button class={`${projectFilterButtonStyles} ${selectedFilter() === "github" ? projectFilterButtonSelectedStyles : ""}`}
+          <button
+            class={`${projectFilterButtonStyles} ${
+              selectedFilter() === "github"
+                ? projectFilterButtonSelectedStyles
+                : ""
+            }`}
             onClick={featuredClickHandler}
           >
             Github
           </button>
-          <button class={`${projectFilterButtonStyles} ${selectedFilter() === "oss" ? projectFilterButtonSelectedStyles : ""}`}
+          <button
+            class={`${projectFilterButtonStyles} ${
+              selectedFilter() === "oss"
+                ? projectFilterButtonSelectedStyles
+                : ""
+            }`}
             onClick={featuredClickHandler}
           >
             OSS
@@ -46,24 +87,7 @@ const Projects = () => {
         </div>
       </div>
       <div class="overflow-y-scroll h-screen shadow-inner p-2">
-        {projectsRes.isLoading ? (
-          <p>loading...</p>
-        ) : (
-          <>
-            {projectsRes.data
-              ?.filter((project) => !project.fork)
-              ?.sort((a, b) => {
-                const dateA = new Date(a.updated_at).valueOf();
-                const dateB = new Date(b.updated_at).valueOf();
-                const value1 = dateA * 0.000000006 + a.commit_count * 0.5;
-                const value2 = dateB * 0.000000006 + b.commit_count * 0.5;
-                return value2 - value1;
-              })
-              .map((project) => (
-                <ProjectCard project={project} />
-              ))}
-          </>
-        )}
+        {projectsRes.isLoading ? <p>loading...</p> : displayedProjects()}
       </div>
     </div>
   );
