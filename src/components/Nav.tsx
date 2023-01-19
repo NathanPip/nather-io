a359fd98bb57d95ecf7db675edcaa46edda87577
NathanPip <107423247+NathanPip@users.noreply.github.com>
2023-01-18 16:23:35 -0800

    project card styling and filtering

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,6 @@
 const Nav = () => {
   return (
-    <nav class="fixed flex justify-end w-full bg-stone-300 shadow-md px-4 py-2">
+    <nav class="fixed flex justify-end w-full bg-stone-300 shadow-md px-4 py-2 z-10">
       <a>github</a>
     </nav>
   );
